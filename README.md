616f3f3c800cf75c859a873a25c9214615bb1bd3
Alireza Nouri <71293106+AlirezaNouri-77@users.noreply.github.com>
2024-07-16 19:05:19 +0330

    migrate to kotlin 2 & version catalog

--- README.md
+++ README.md
@@ -5,7 +5,6 @@
 - Media3 Session Service
 - Kotlin
 - Jetpack Compose
-- Koin for DI
 - Navigation
 - Flow
 - Coroutine
@@ -14,8 +13,3 @@
 - **Model:** Manage data, business logic, and interactions with data sources.
 - **View:** Build the UI using Jetpack Compose and observe ViewModel state changes.
 - **ViewModel:** Intermediary between Model and View, handling user interactions and updating the Model.
-
-### ScreenShot
-<img src="https://github.com/AlirezaNouri-77/Media-Player-Compose/assets/71293106/03cfa469-7628-4d85-a5bf-9ac17f761eaa" width="200" height="400">  <img src="https://github.com/AlirezaNouri-77/Media-Player-Compose/assets/71293106/9b9ddc2c-693f-4ed6-bbe0-425176ae3199" width="200" height="400">  <img src="https://github.com/AlirezaNouri-77/Media-Player-Compose/assets/71293106/88965f17-7841-439f-805d-67b780c61928" width="200" height="400">  <img src="https://github.com/AlirezaNouri-77/Media-Player-Compose/assets/71293106/1a0cc3f1-8102-4751-8838-70fbdb05e88b" width="200" height="400">
-
-<img src="https://github.com/AlirezaNouri-77/Media-Player-Compose/assets/71293106/39f33479-5241-408a-8114-73029b2d051a" width="200" height="400">  <img src="https://github.com/AlirezaNouri-77/Media-Player-Compose/assets/71293106/a04a5f55-5daa-423b-9d26-f85e4c33d170" width="200" height="400">  <img src="https://github.com/AlirezaNouri-77/Media-Player-Compose/assets/71293106/5b38d0c9-618e-442a-9252-f8796f7237c0" width="200" height="400">
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,94 +1,86 @@
 plugins {
-  id("com.android.application")
-  id("org.jetbrains.kotlin.android")
-  id("com.google.devtools.ksp")
+  alias(libs.plugins.android.application)
+  alias(libs.plugins.android.kotlin)
+  alias(libs.plugins.android.ksp)
+  alias(libs.plugins.android.compose.plugin)
 }
 
 android {
   namespace = "com.example.mediaplayerjetpackcompose"
   compileSdk = 34
-  
+
   defaultConfig {
-	applicationId = "com.example.mediaplayerjetpackcompose"
-	minSdk = 28
-	targetSdk = 34
-	versionCode = 1
-	versionName = "1.0"
-	
-	testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-	vectorDrawables {
-	  useSupportLibrary = true
-	}
+    applicationId = "com.example.mediaplayerjetpackcompose"
+    minSdk = 28
+    targetSdk = 34
+    versionCode = 1
+    versionName = "1.0"
+
+    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    vectorDrawables {
+      useSupportLibrary = true
+    }
   }
-  
+
   buildTypes {
-	release {
-	  isMinifyEnabled = false
-	  proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-	}
+    release {
+      isMinifyEnabled = false
+      proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+    }
   }
   compileOptions {
-	sourceCompatibility = JavaVersion.VERSION_1_8
-	targetCompatibility = JavaVersion.VERSION_1_8
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
   }
   kotlinOptions {
-	jvmTarget = "1.8"
+    jvmTarget = "1.8"
   }
   buildFeatures {
-	compose = true
+    compose = true
   }
-  composeOptions {
-	kotlinCompilerExtensionVersion = "1.5.10"
-  }
+
   packaging {
-	resources {
-	  excludes += "/META-INF/{AL2.0,LGPL2.1}"
-	}
+    resources {
+      excludes += "/META-INF/{AL2.0,LGPL2.1}"
+    }
   }
 }
 
 dependencies {
-  
-  val media3Version = "1.2.1"
-  val navVersion = "2.7.7"
-  val lifecycleVersion = "2.7.0"
-  val constraintLayout = "1.0.1"
-  val roomVersion = "2.6.1"
-  val koinAndroidComposeVersion = "3.5.3"
 
-  implementation("androidx.core:core-ktx:1.12.0")
-  implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleVersion")
-  implementation("androidx.activity:activity-compose:1.8.2")
-  implementation(platform("androidx.compose:compose-bom:2024.02.01"))
-  implementation("androidx.compose.ui:ui")
-  implementation("androidx.compose.ui:ui-graphics")
-  implementation("androidx.compose.ui:ui-tooling-preview")
-  implementation("androidx.compose.material3:material3:1.2.0")
-  testImplementation("junit:junit:4.13.2")
-  androidTestImplementation("androidx.test.ext:junit:1.1.5")
-  androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-  androidTestImplementation(platform("androidx.compose:compose-bom:2024.02.01"))
-  androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-  debugImplementation("androidx.compose.ui:ui-tooling")
-  debugImplementation("androidx.compose.ui:ui-test-manifest")
-  
-  implementation ("androidx.media3:media3-exoplayer:$media3Version")
-  implementation ("androidx.media3:media3-exoplayer-dash:$media3Version")
-  implementation ("androidx.media3:media3-ui:$media3Version")
-  implementation("androidx.media3:media3-session:$media3Version")
-  
-  implementation("androidx.lifecycle:lifecycle-runtime-compose:$lifecycleVersion")
-  implementation("androidx.lifecycle:lifecycle-viewmodel-compose:$lifecycleVersion")
-  
-  implementation("androidx.navigation:navigation-compose:$navVersion")
-  
-  implementation("androidx.constraintlayout:constraintlayout-compose:$constraintLayout")
+  implementation(libs.androidx.core.ktx)
+  implementation(libs.androidx.activity.compose)
+  implementation(libs.androidx.compose.bom)
+  implementation(libs.androidx.ui)
+  implementation(libs.androidx.ui.graphics)
+  implementation(libs.androidx.ui.tooling.preview)
+  implementation(libs.androidx.material3)
+  debugImplementation(libs.androidx.ui.tooling)
 
-  implementation ("androidx.room:room-ktx:$roomVersion")
-  implementation ("androidx.room:room-runtime:$roomVersion")
-  ksp("androidx.room:room-compiler:$roomVersion")
+  testImplementation(libs.junit)
+  androidTestImplementation(libs.androidx.junit)
+  androidTestImplementation(libs.androidx.espresso.core)
+  androidTestImplementation(libs.androidx.ui.test.junit4)
+  debugImplementation(libs.androidx.ui.test.manifest)
 
-  implementation ("io.insert-koin:koin-android:$koinAndroidComposeVersion")
-  implementation ("io.insert-koin:koin-androidx-compose:$koinAndroidComposeVersion")
+  implementation(libs.androidx.media3.exoplayer)
+  implementation(libs.androidx.media3.exoplayer.dash)
+  implementation(libs.androidx.media3.ui)
+  implementation(libs.androidx.media3.session)
+
+  implementation(libs.androidx.lifecycle.runtime.ktx)
+  implementation(libs.androidx.lifecycle.runtime.compose)
+  implementation(libs.androidx.lifecycle.viewmodel.compose)
+
+  implementation(libs.androidx.navigation.compose)
+
+  implementation(libs.androidx.constraintlayout.compose)
+
+  implementation(libs.androidx.room.ktx)
+  implementation(libs.androidx.room.runtime)
+  ksp(libs.androidx.room.compiler)
+
+  implementation(libs.koin.android)
+  implementation(libs.koin.androidx.compose)
 
 }
