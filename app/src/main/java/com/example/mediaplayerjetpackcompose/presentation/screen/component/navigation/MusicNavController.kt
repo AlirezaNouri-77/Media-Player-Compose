614b99d00738d85906c537e8222aa0eeaaf20d32
unknown <shermanrex98@gmail.com>
2024-10-31 19:57:42 +0330

    close full screen music player when press back

--- app/src/main/java/com/example/mediaplayerjetpackcompose/presentation/screen/component/navigation/MusicNavController.kt
+++ app/src/main/java/com/example/mediaplayerjetpackcompose/presentation/screen/component/navigation/MusicNavController.kt
@@ -5,30 +5,22 @@ import android.os.Build
 import android.view.Window
 import android.view.WindowInsets
 import android.view.WindowInsetsController
+import androidx.activity.compose.BackHandler
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
-import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.combinedClickable
-import androidx.compose.foundation.gestures.detectDragGestures
-import androidx.compose.foundation.gestures.detectVerticalDragGestures
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.offset
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.blur
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavDestination.Companion.hasRoute
 import androidx.navigation.NavHostController
@@ -64,6 +56,12 @@ fun MusicNavController(
   }
   LaunchedEffect(key1 = currentMusicState.metaData) {
     musicPageViewModel.getColorPaletteFromArtwork(currentMusicState.uri)
+  }
+
+  BackHandler {
+    if (musicPageViewModel.isFullPlayerShow){
+      musicPageViewModel.isFullPlayerShow = false
+    }
   }
 
   if (currentRoute?.hasRoute(MainScreenNavigationModel.VideoPlayerScreen::class) == true) {
