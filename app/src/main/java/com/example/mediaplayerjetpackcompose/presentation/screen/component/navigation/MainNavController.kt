2073f4b6541da76daeae36455ad06b9735c8aa04
unknown <shermanrex98@gmail.com>
2024-12-18 19:09:03 +0330

    code cleanup

--- app/src/main/java/com/example/mediaplayerjetpackcompose/presentation/screen/component/navigation/MainNavController.kt
+++ app/src/main/java/com/example/mediaplayerjetpackcompose/presentation/screen/component/navigation/MainNavController.kt
@@ -2,8 +2,6 @@ package com.example.mediaplayerjetpackcompose.presentation.screen.component.navi
 
 import android.app.Activity
 import android.os.Build
-import android.os.Bundle
-import android.util.Log
 import android.view.Window
 import android.view.WindowInsets
 import android.view.WindowInsetsController
@@ -14,20 +12,16 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.produceState
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.navigation.NavController
-import androidx.navigation.NavDestination
 import androidx.navigation.NavDestination.Companion.hasRoute
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.serialization.generateHashCode
 import androidx.navigation.toRoute
 import com.example.mediaplayerjetpackcompose.domain.model.navigation.MainScreenNavigationModel
 import com.example.mediaplayerjetpackcompose.presentation.screen.music.MusicPageViewModel
