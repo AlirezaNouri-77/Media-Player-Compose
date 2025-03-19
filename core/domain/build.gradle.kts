42a1315c15ca7335f57c683e0574ec6b8b82f6dc
Alireza Nouri <shermanrex98@gmail.com>
2025-03-19 20:28:32 +0330

    adapt core:domain to convention plugin

--- core/domain/build.gradle.kts
+++ core/domain/build.gradle.kts
@@ -1,41 +1,15 @@
 plugins {
-  alias(libs.plugins.android.library)
-  alias(libs.plugins.android.kotlin)
+  alias(libs.plugins.mediaplayer.androidConventionPlugin)
 }
 
 android {
   namespace = "com.example.core.domain"
-  compileSdk = libs.versions.compileSdk.get().toInt()
-
-  defaultConfig {
-    minSdk = libs.versions.minSdk.get().toInt()
-
-    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    consumerProguardFiles("consumer-rules.pro")
-  }
-
-  buildTypes {
-    release {
-      isMinifyEnabled = false
-      proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-    }
-  }
-  compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
-  }
-  kotlinOptions {
-    jvmTarget = "11"
-  }
 }
 
 dependencies {
 
-  implementation(libs.androidx.core.ktx)
-  testImplementation(libs.junit)
-  androidTestImplementation(libs.androidx.junit)
-  androidTestImplementation(libs.androidx.espresso.core)
-
   implementation(project(":core:model"))
+
+  implementation(libs.jetbrains.kotlinx.coroutine)
 
 }
