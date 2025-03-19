e57bfbe3a09cb1d712ef0be2981de50450325275
Alireza Nouri <shermanrex98@gmail.com>
2025-03-19 19:49:26 +0330

    adapt core:database to convention plugin

--- core/database/build.gradle.kts
+++ core/database/build.gradle.kts
@@ -1,33 +1,10 @@
 plugins {
-  alias(libs.plugins.android.library)
-  alias(libs.plugins.android.kotlin)
+  alias(libs.plugins.mediaplayer.androidConventionPlugin)
   alias(libs.plugins.mediaplayer.roomConventionPlugin)
 }
 
 android {
   namespace = "com.example.core.database"
-  compileSdk = libs.versions.compileSdk.get().toInt()
-
-  defaultConfig {
-    minSdk = libs.versions.minSdk.get().toInt()
-
-    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    consumerProguardFiles("consumer-rules.pro")
-  }
-
-  buildTypes {
-    release {
-      isMinifyEnabled = false
-      proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-    }
-  }
-  compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
-  }
-  kotlinOptions {
-    jvmTarget = "11"
-  }
 }
 
 dependencies {
