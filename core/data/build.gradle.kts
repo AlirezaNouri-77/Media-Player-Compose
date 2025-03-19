70c4bbfc0cbcd08d0109d2820a90b657f024f12f
Alireza Nouri <shermanrex98@gmail.com>
2025-03-19 20:28:54 +0330

    adapt core:data to convention plugin

--- core/data/build.gradle.kts
+++ core/data/build.gradle.kts
@@ -1,38 +1,15 @@
 plugins {
-  alias(libs.plugins.android.library)
-  alias(libs.plugins.android.kotlin)
+  alias(libs.plugins.mediaplayer.androidConventionPlugin)
 }
 
 android {
   namespace = "com.example.core.data"
-  compileSdk = libs.versions.compileSdk.get().toInt()
-
-  defaultConfig {
-    minSdk = libs.versions.minSdk.get().toInt()
-
-    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    consumerProguardFiles("consumer-rules.pro")
-  }
-
-  buildTypes {
-    release {
-      isMinifyEnabled = false
-      proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-    }
-  }
-  compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
-  }
-  kotlinOptions {
-    jvmTarget = "11"
-  }
 }
 
 dependencies {
 
-  implementation(project(":core:common"))
   implementation(project(":core:database"))
+  implementation(project(":core:common"))
   implementation(project(":core:domain"))
   implementation(project(":core:model"))
 
